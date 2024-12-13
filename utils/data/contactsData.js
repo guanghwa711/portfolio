f35b989724e2a9069aec4d4657c1528ebbf9e604
guanghwa <guanghwa26@gmail.com>
2024-12-13 11:34:55 +0200

    initial

---
+++ utils/data/contactsData.js
@@ -0,0 +1,11 @@
+export const contactsData = {
+    email: 'guanghwa26@gmail.com',
+    phone: '+8801608797655',
+    address: '258 Queens Road East, Wan Chai, Hong Kong',
+    github: 'https://github.com/guanghwa',
+    facebook: 'https://www.facebook.com/abusaid.riyaz/',
+    linkedIn: 'https://www.linkedin.com/in/abu-said-bd/',
+    twitter: 'https://twitter.com/said7388',
+    stackOverflow: 'https://stackoverflow.com/users/16840768/abu-said',
+    devUsername: "guanghwa"
+}
---
+++ utils/data/educations.js
@@ -0,0 +1,14 @@
+export const educations = [
+  {
+    id: 1,
+    title: "Bachelor Degree in Computer Science",
+    duration: "2012 - 06/2016",
+    institution: "The University of Hong Kong",
+  },
+  {
+    id: 2,
+    title: "Master of Science in Data Science",
+    duration: "2018 - 2020",
+    institution: "Hong Kong University of Science and Technology (HKUST)",
+  },
+]
